996a4391384a7956511098a6917ed16467c95945
Kalinovsky Nikita <nkalinovsky@gmail.com>
2024-10-28 15:03:33 +0200

    share section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,6 +25,36 @@ const Home = () => {
                         #FurniroFurniture
                     </h3>
                 </div>
+
+                <div className="grid grid-cols-12 grid-rows-2 gap-4">
+                    <div className="row-span-1 col-span-2 self-end">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-1.jpg" alt="" />
+                    </div>
+                    <div className="row-start-1 col-span-3 col-start-3 self-end">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-2.jpg" alt="" />
+                    </div>
+                    <div className="col-start-1 col-span-3 row-start-2 self-start">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-3.jpg" alt="" />
+                    </div>
+                    <div className="col-start-4 col-span-2 row-start-2 row-span-1 self-start">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-4.jpg" alt="" />
+                    </div>
+                    <div className="col-start-6 col-span-2 row-span-2 self-center">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-5.jpg" alt="" />
+                    </div>
+                    <div className="col-start-8 col-span-2 row-span-1 self-end">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-6.jpg" alt="" />
+                    </div>
+                    <div className="col-start-8 col-span-1 row-start-2 row-span-1 self-start">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-7.jpg" alt="" />
+                    </div>
+                    <div className="col-start-10 col-span-3 row-span-1 self-end">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-8.jpg" alt="" />
+                    </div>
+                    <div className="col-start-9 col-span-2 row-span-1 self-start">
+                        <img className="w-full" src="https://raw.githubusercontent.com/Kalynovskyi/furniro-store/refs/heads/master/src/assets/images/home-share-screen-9.jpg" alt="" />
+                    </div>
+                </div>
             </section>
         </>
     );
